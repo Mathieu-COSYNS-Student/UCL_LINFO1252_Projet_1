a2a8ad710ee7f0a851c1cbefa172bcbfc98df152
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2021-11-29 15:30:31 +0100

    Producteurs_consomateurs

--- prodcons/producteurs_consommateurs.c
+++ prodcons/producteurs_consommateurs.c
@@ -9,8 +9,11 @@
 #define BUFFER_SIZE 8
 #define ROUNDS 1024
 
+int producer_round_number = 0;
+pthread_mutex_t producer_round_mutex;
 
-
+int consumer_round_number = 0;
+pthread_mutex_t consumer_round_mutex;
 
 pthread_mutex_t mutex;
 
@@ -30,19 +33,51 @@ int out = 0;
 
 
 
+bool can_produce()
+{
+  bool can_produce = false;
+
+  pthread_mutex_lock(&producer_round_mutex);
+  if (producer_round_number < ROUNDS) {
+    producer_round_number++;
+    can_produce = true;
+  }
+  pthread_mutex_unlock(&producer_round_mutex);
+
+  return can_produce;
+}
+
+bool can_consume()
+{
+  bool can_consume = false;
+
+  pthread_mutex_lock(&consumer_round_mutex);
+  if (consumer_round_number < ROUNDS) {
+    consumer_round_number++;
+    can_consume = true;
+  }
+  pthread_mutex_unlock(&consumer_round_mutex);
+
+  return can_consume;
+}
+
+
+
 void* producer(void* arg)
 {
-  for (int i = 0; i < ROUNDS; ++i)
+  int* num = (int*) arg;
+
+  while (can_produce())
   {
     sem_wait(&empty); // WAITING FOR AN EMPTY SLOT
     pthread_mutex_lock(&mutex);
-     // section critique
-    printf("Producing in slot n° %d\n",in);
+    // section critique
+    printf("Producer %d: Producing in slot n° %d\n",*num,in);
     buffer[in] = 1;
+    while(rand() > RAND_MAX/10000);
     in = (in+1)%BUFFER_SIZE;
     pthread_mutex_unlock(&mutex);
     sem_post(&full); // INDICATING THAT A SPOT HAS BEEN FILLED
-    while(rand() > RAND_MAX/10000);
   }
   return (NULL);
 }
@@ -56,13 +91,14 @@ void* producer(void* arg)
 
 void* consumer(void* arg)
 {
+  int* num = (int*) arg;
  
- for (int i = 0; i < ROUNDS; i++)
+ while (can_consume())
  {
    sem_wait(&full); // WAITING FOR AN OCCUPIED SPOT
    pthread_mutex_lock(&mutex);
     // section critique
-   printf("Consuming from slot n° %d\n",out);
+   printf("Consumer %d: Consuming from slot n° %d\n",*num,out);
    buffer[out]= 0;
    out = (out+1)%BUFFER_SIZE;
    pthread_mutex_unlock(&mutex);
@@ -121,6 +157,22 @@ int main(int argc, char const *argv[])
       printf("MUTEX CREATED\n");
   }
 
+  err = pthread_mutex_init(&producer_round_mutex, NULL);
+  if(err!=0){
+      printf("ERROR WHILE INITIALIZING THE MUTEX");
+      exit(-1);
+  }else{
+      printf("MUTEX CREATED\n");
+  }
+
+  err = pthread_mutex_init(&consumer_round_mutex, NULL);
+  if(err!=0){
+      printf("ERROR WHILE INITIALIZING THE MUTEX");
+      exit(-1);
+  }else{
+      printf("MUTEX CREATED\n");
+  }
+
   err = sem_init(&empty, 0 , BUFFER_SIZE); 
   if(err!=0){
       printf("ERROR WHILE INITIALIZING THE EMPTY SEMAPHORE");
@@ -144,7 +196,7 @@ for (int i = 0; i < numberOfProducers; i++)
   {
     int* j = (int*)malloc(sizeof(int));
     *j = i;
-    err = pthread_create(&(producers[i]),NULL,&producer,NULL);
+    err = pthread_create(&(producers[i]),NULL,&producer, (void*)j);
     if(err!=0){
       printf("ERROR WHILE CREATING PRODUCER THREAD N°%d", i);
       exit(-1);
@@ -157,7 +209,7 @@ for (int i = 0; i < numberOfConsumers; i++)
   {
     int* j = (int*)malloc(sizeof(int));
     *j = i;
-    err = pthread_create(&(consumers[i]),NULL,&consumer,NULL);
+    err = pthread_create(&(consumers[i]),NULL,&consumer, (void*)j);
     if(err!=0){
       printf("ERROR WHILE CREATING CONSUMER THREAD N°%d", i);
       exit(-1);
