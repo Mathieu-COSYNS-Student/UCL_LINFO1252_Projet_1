4d2b31d6c80eda079f80234243a20eca42cfff2e
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2021-12-02 15:02:05 +0100

    Output for benchmark-post in file

--- benchmark/run-all_post_processor.sh
+++ benchmark/run-all_post_processor.sh
@@ -7,30 +7,33 @@ OUTDIR="$BASEDIR/out"
 
 # Tache 1.1
 "$BASEDIR/test_time_post_processor.py" \
-  -t "Philosophes" \
+  -t "Problème des philosophes" \
   -x "Nombre de threads totaux" \
   -y "Nombre de secondes" \
-  -i "$OUTDIR/philosophes_1.csv" -m 1 \
-  -i "$OUTDIR/philosophes_2.csv" -m 2 \
-  -i "$OUTDIR/philosophes_3.csv" -m 3
+  -i "$OUTDIR/philosophes_1.csv" -m "Synchronisation POSIX" \
+  -i "$OUTDIR/philosophes_2.csv" -m "Synchronisation avec test-and-set" \
+  -i "$OUTDIR/philosophes_3.csv" -m "Synchronisation avec test-and-test-and-set" \
+  -o "$OUTDIR/philosophes"
 
 # Tache 1.2
 "$BASEDIR/test_time_post_processor.py" \
-  -t "Producteurs-consomateur" \
+  -t "Problème des producteurs-consommateurs" \
   -x "Nombre de threads totaux" \
   -y "Nombre de secondes" \
-  -i "$OUTDIR/producteurs_consommateurs_1.csv" -m 1 \
-  -i "$OUTDIR/producteurs_consommateurs_2.csv" -m 2 \
-  -i "$OUTDIR/producteurs_consommateurs_3.csv" -m 3
+  -i "$OUTDIR/producteurs_consommateurs_1.csv" -m "Synchronisation POSIX" \
+  -i "$OUTDIR/producteurs_consommateurs_2.csv" -m "Synchronisation avec test-and-set" \
+  -i "$OUTDIR/producteurs_consommateurs_3.csv" -m "Synchronisation avec test-and-test-and-set" \
+  -o "$OUTDIR/producteurs_consommateurs"
 
 # Tache 1.3
 "$BASEDIR/test_time_post_processor.py" \
-  -t "Lecteurs et érivains" \
+  -t "Problème des lecteurs et écrivains" \
   -x "Nombre de threads totaux" \
   -y "Nombre de secondes" \
-  -i "$OUTDIR/readers_writers_1.csv" -m 1 \
-  -i "$OUTDIR/readers_writers_2.csv" -m 2 \
-  -i "$OUTDIR/readers_writers_3.csv" -m 3
+  -i "$OUTDIR/readers_writers_1.csv" -m "Synchronisation POSIX" \
+  -i "$OUTDIR/readers_writers_2.csv" -m "Synchronisation avec test-and-set" \
+  -i "$OUTDIR/readers_writers_3.csv" -m "Synchronisation avec test-and-test-and-set" \
+  -o "$OUTDIR/readers_writers"
   
 # Tache 2.2
 "$BASEDIR/test_time_post_processor.py" \
@@ -39,3 +42,4 @@ OUTDIR="$BASEDIR/out"
   -y "Nombre de secondes" \
   -i "$OUTDIR/lock_simple_test_1.csv" -m "test-and-set" \
   -i "$OUTDIR/lock_simple_test_2.csv" -m "test-and-test-and-set" \
+  -o "$OUTDIR/lock_simple_test"
