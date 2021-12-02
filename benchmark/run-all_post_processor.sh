014e7dc1b51036fa9a38242a8af6fe52586c67e2
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2021-12-02 15:14:53 +0100

    Add question for graphical interface

--- benchmark/run-all_post_processor.sh
+++ benchmark/run-all_post_processor.sh
@@ -4,6 +4,11 @@ echo "Benchmark-post running..."
 
 BASEDIR=$(dirname $0)
 OUTDIR="$BASEDIR/out"
+GRAPHICS="--no-graphics"
+
+if "$BASEDIR/yes-no.sh" -m "Do you want to see charts in a graphical interface ?"; then
+  GRAPHICS=""
+fi
 
 # Tache 1.1
 "$BASEDIR/test_time_post_processor.py" \
@@ -13,7 +18,7 @@ OUTDIR="$BASEDIR/out"
   -i "$OUTDIR/philosophes_1.csv" -m "Synchronisation POSIX" \
   -i "$OUTDIR/philosophes_2.csv" -m "Synchronisation avec test-and-set" \
   -i "$OUTDIR/philosophes_3.csv" -m "Synchronisation avec test-and-test-and-set" \
-  -o "$OUTDIR/philosophes"
+  -o "$OUTDIR/philosophes" "$GRAPHICS" &
 
 # Tache 1.2
 "$BASEDIR/test_time_post_processor.py" \
@@ -23,7 +28,7 @@ OUTDIR="$BASEDIR/out"
   -i "$OUTDIR/producteurs_consommateurs_1.csv" -m "Synchronisation POSIX" \
   -i "$OUTDIR/producteurs_consommateurs_2.csv" -m "Synchronisation avec test-and-set" \
   -i "$OUTDIR/producteurs_consommateurs_3.csv" -m "Synchronisation avec test-and-test-and-set" \
-  -o "$OUTDIR/producteurs_consommateurs"
+  -o "$OUTDIR/producteurs_consommateurs" "$GRAPHICS" &
 
 # Tache 1.3
 "$BASEDIR/test_time_post_processor.py" \
@@ -33,7 +38,7 @@ OUTDIR="$BASEDIR/out"
   -i "$OUTDIR/readers_writers_1.csv" -m "Synchronisation POSIX" \
   -i "$OUTDIR/readers_writers_2.csv" -m "Synchronisation avec test-and-set" \
   -i "$OUTDIR/readers_writers_3.csv" -m "Synchronisation avec test-and-test-and-set" \
-  -o "$OUTDIR/readers_writers"
+  -o "$OUTDIR/readers_writers" "$GRAPHICS" &
   
 # Tache 2.2
 "$BASEDIR/test_time_post_processor.py" \
@@ -42,4 +47,6 @@ OUTDIR="$BASEDIR/out"
   -y "Nombre de secondes" \
   -i "$OUTDIR/lock_simple_test_1.csv" -m "test-and-set" \
   -i "$OUTDIR/lock_simple_test_2.csv" -m "test-and-test-and-set" \
-  -o "$OUTDIR/lock_simple_test"
+  -o "$OUTDIR/lock_simple_test" "$GRAPHICS" &
+
+wait $(jobs -p)
